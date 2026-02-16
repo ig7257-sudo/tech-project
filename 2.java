38fe5e847f62734e42f02f00af94d9d1fb6f1396
ig7257-sudo <ig7257@srmist.edu.in>
2026-02-16 09:20:38 +0530

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,9 @@
+
+
+System.out.println("   ***   " + "   " + "  *****  ");
+System.out.println("  ** **  " + "   " + " **      ");
+System.out.println(" **   ** " + "   " + " **      ");
+System.out.println(" **   ** " + "   " + "  *****  ");
+System.out.println(" **   ** " + "   " + "      ** ");
+System.out.println("  ** **  " + "   " + " **   ** ");
+System.out.println("   ***   " + "   " + "  *****  ");
